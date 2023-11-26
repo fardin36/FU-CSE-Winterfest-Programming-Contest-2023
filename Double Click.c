7407c0464da9eb85de3fdd3d567e5be195af5424
Baha Uddin Faruqi <138319867+fardin36@users.noreply.github.com>
2023-11-26 21:29:51 +0600

    Update Double Click.c

--- Double Click.c
+++ Double Click.c
@@ -21,3 +21,26 @@ int main()
     printf("%d\n", t);
     return 0;
 }
+
+
+
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int n, d, t = -1;
+//     scanf("%d %d", &n, &d);
+//     int time[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &time[i]);
+//         if (i > 0)
+//         {
+//             if (time[i] - time[i - 1] <= d && t == -1)
+//                 t = time[i];
+//         }
+//     }
+//     printf("%d\n", t);
+//     return 0;
+// }
